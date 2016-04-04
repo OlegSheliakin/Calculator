2b712b06068c5d25e28cb739b81f57613492c0cd
Oleg Sheliakin <olegsheliakin@gmail.com>
2016-04-05 00:43:23 +0300

    05.04 added IOperations, Impl

---
+++ app/src/main/java/home/oleg/calculator/Impl/Multiplication.java
@@ -0,0 +1,26 @@
+package home.oleg.calculator.Impl;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+import home.oleg.calculator.Interfaces.IOperations;
+
+/**
+ * Created by Oleg on 05.04.2016.
+ */
+public class Multiplication implements IOperations{
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public double evaluate(BigDecimal... args) {
+        return args[0].multiply(args[1], MathContext.DECIMAL32).doubleValue();
+    }
+
+    @Override
+    public int getPiority() {
+        return 2;
+    }
+}
